9c83b5355bce5bd7a020561eff852ac6251fa3f0
francessergi <60177653+francessergi@users.noreply.github.com>
2020-11-13 02:25:56 +0100

    Añadido JavaScript
    Pequeños detalles añadidos con JS.
    Efectos Hover.
    Nuevos enlaces.

--- UF01 - HTML/Diseño23/app.js
+++ UF01 - HTML/Diseño23/app.js
@@ -3,12 +3,11 @@ const navSlide = () => {
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
 
-
     burger.addEventListener('click', () => {
-        //Toggle Nav
+        //Toggle Nav: Fa que aparegui la barra lateral
         nav.classList.toggle('nav-active');
 
-        //Animate links
+        //Animate links: Fa que apareguin els enllaços
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
                 link.style.animation = '';
@@ -17,11 +16,28 @@ const navSlide = () => {
             }
         });
 
-        //Burger animation
+        //Burger animation: fa que l'amburguesa roti
         burger.classList.toggle('toggle');
 
     });
-
 }
 
 navSlide();
+
+var imatges = ["url(./img/casa1.jpg)", "url(./img/casa2.jpg)", "url(./img/casa3.jpg)", "url(./img/casa4.jpg)", "url(./img/casa5.jpg)", "url(./img/casa6.jpg)", ]
+
+var contador = 4;
+
+document.getElementById("flechaIzquierda").addEventListener('click', () => {
+    contador--;
+    if (contador == 0)
+        contador = 6;
+    document.getElementById("properties").style.backgroundImage = imatges[contador];
+});
+
+document.getElementById("flechaDerecha").addEventListener('click', () => {
+    contador++;
+    if (contador == 7)
+        contador = 1;
+    document.getElementById("properties").style.backgroundImage = imatges[contador];
+});
