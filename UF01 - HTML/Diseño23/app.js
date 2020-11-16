d8af70152833da1e567fa4b2505cea07777cb613
francessergi <60177653+francessergi@users.noreply.github.com>
2020-11-16 17:14:20 +0100

    Practica2c + Modificación CSS (- grid)

--- UF01 - HTML/Diseño23/app.js
+++ UF01 - HTML/Diseño23/app.js
@@ -26,18 +26,37 @@ navSlide();
 
 var imatges = ["url(./img/casa1.jpg)", "url(./img/casa2.jpg)", "url(./img/casa3.jpg)", "url(./img/casa4.jpg)", "url(./img/casa5.jpg)", "url(./img/casa6.jpg)", ]
 
-var contador = 4;
+var contador = 0;
 
 document.getElementById("flechaIzquierda").addEventListener('click', () => {
     contador--;
-    if (contador == 0)
-        contador = 6;
+    if (contador < 0) {
+        contador = 5;
+    }
+
     document.getElementById("properties").style.backgroundImage = imatges[contador];
 });
 
 document.getElementById("flechaDerecha").addEventListener('click', () => {
+
     contador++;
-    if (contador == 7)
-        contador = 1;
+    if (contador > 5) {
+        contador = 0;
+    }
+
     document.getElementById("properties").style.backgroundImage = imatges[contador];
+
 });
+
+
+function changeImages() {
+    document.getElementById("properties").style.backgroundImage = imatges[contador];
+    if (contador < imatges.length - 1) {
+        contador++;
+    } else {
+        contador = 0;
+    }
+    setTimeout("changeImages()", 4000);
+}
+
+changeImages();
