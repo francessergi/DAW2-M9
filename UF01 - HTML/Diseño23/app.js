ae5edc97e19cc60cf800cc3731cc2863d57b2fd3
francessergi <60177653+francessergi@users.noreply.github.com>
2020-11-25 15:36:45 +0100

    Arreglo burger + pequeñeces

--- UF01 - HTML/Diseño23/app.js
+++ UF01 - HTML/Diseño23/app.js
@@ -1,29 +1,3 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-    burger.addEventListener('click', () => {
-        //Toggle Nav: Fa que aparegui la barra lateral
-        nav.classList.toggle('nav-active');
-
-        //Animate links: Fa que apareguin els enllaços
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-
-        //Burger animation: fa que l'amburguesa roti
-        burger.classList.toggle('toggle');
-
-    });
-}
-
-navSlide();
-
 var imatges = ["url(./img/casa1.jpg)", "url(./img/casa2.jpg)", "url(./img/casa3.jpg)", "url(./img/casa4.jpg)", "url(./img/casa5.jpg)", "url(./img/casa6.jpg)", ]
 
 var contador = 0;
